f4cdb5058f00444954b1ed62b8bc77ae81128c14
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2020-09-08 20:57:13 +0900

    jsx, proptypes example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name}) {
+	return <h1> I like {name}</h1>
+}
+
+const foodILike = [
+	{
+		name : 'kimchi',
+		image : 'ttt'
+	},
+	{
+		name : 'kimchi1',
+		image : 'ttt'
+	},
+	{
+		name : 'kimchi2',
+		image : 'ttt'
+	},
+	{
+		name : 'kimchi3',
+		image : 'ttt'
+	}
+];
+
+function renderFood(dish){
+	return <Food name={dish.name}> </Food>
+}
+
+Food.PropTypes = {
+	name : PropTypes.string.isRequired, 
+}
 
 function App() {
   return (
-    <div className="App"> hello!
+    <div> 
+		{foodILike.map(renderFood)}
     </div>
   );
 }
